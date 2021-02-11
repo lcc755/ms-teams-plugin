4bc58ee8d0182cd5240176a2d4013c249bcdccfa
Todimu Jenrola <todimujenrola@gmail.com>
2021-02-11 12:10:20 -0500

    Add files via upload

---
+++ src/components/Personal.js
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import React from 'react';
+import './App.css';
+import * as microsoftTeams from "@microsoft/teams-js";
+
+/**
+ * The 'GroupTab' component renders the main tab content
+ * of your app.
+ */
+class Tab extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      context: {}
+    }
+  }
+
+  //React lifecycle method that gets called once a component has finished mounting
+  //Learn more: https://reactjs.org/docs/react-component.html#componentdidmount
+  componentDidMount(){
+    // Get the user context from Teams and set it in the state
+    microsoftTeams.getContext((context, error) => {
+      this.setState({
+        context: context
+      });
+    });
+    // Next steps: Error handling using the error object
+  }
+
+  render() {
+     let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
+
+            return(
+        
+            <div>
+              <h1>Important Contacts</h1>
+                <ul>
+                  <li>Help Desk: <a href="mailto:support@company.com">support@company.com</a></li>
+                  <li>Human Resources: <a href="mailto:hr@company.com">hr@company.com</a></li>
+                  <li>Facilities: <a href="mailto:facilities@company.com">facilities@company.com</a></li>
+                </ul>
+            </div>
+            );
+  }
+}
+export default Tab;
